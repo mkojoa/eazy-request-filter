a06cb1fcb8f549d4c9af4c90fceaee63161bf812
mkojoa <michael.ameyaw@persol.net>
2021-05-04 16:39:46 +0000

    init with project startup

---
+++ src/eazy.request.filter/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace eazy.request.filter
+{
+    public static class Extensions
+    {
+
+        public static IServiceCollection AddEazyRequestFilter<TContext>(this IServiceCollection services,
+            IConfiguration configuration) 
+            where TContext : DbContext
+        {
+            //services.AddEfCore<TContext>(configuration);
+            //services.AddCacheable(configuration);
+
+            return services;
+        }
+         
+        public static IApplicationBuilder UseEazyRequestFilter(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            configuration.GetSection("");
+
+            return app;
+        }
+    }
+}
